a13e8fc40bc8f4fc40037b3858d3c57e40ac4b46
Dan Williams <dan@danwilliams.co>
2014-05-17 13:22:27 +0100

    Initial Commit

---
+++ DJWSegmentedViewController/DJWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DJWAppDelegate.h
+//  DJWSegmentedViewController
+//
+//  Created by Dan Williams on 17/05/2014.
+//  Copyright (c) 2014 Dan Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DJWSegmentedViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DJWSegmentedViewController
+//
+//  Created by Dan Williams on 17/05/2014.
+//  Copyright (c) 2014 Dan Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DJWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DJWAppDelegate class]));
+    }
+}
