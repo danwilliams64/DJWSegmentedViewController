e3cb8c417907a034731e6a22ba48b4ecb0d768a9
Dan Williams <dan@danwilliams.co>
2014-05-17 13:34:09 +0100

    Initial commit.

---
+++ DJWSegmentedViewController/DJWSegmentedViewController.h
@@ -0,0 +1,13 @@
+//
+//  DJWSegmentedViewController.h
+//  DJWSegmentedViewController
+//
+//  Created by Dan Williams on 17/05/2014.
+//  Copyright (c) 2014 Dan Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJWSegmentedViewController : UIViewController
+
+@end
---
+++ DJWSegmentedViewController/DJWSegmentedViewController.m
@@ -0,0 +1,49 @@
+//
+//  DJWSegmentedViewController.m
+//  DJWSegmentedViewController
+//
+//  Created by Dan Williams on 17/05/2014.
+//  Copyright (c) 2014 Dan Williams. All rights reserved.
+//
+
+#import "DJWSegmentedViewController.h"
+
+@interface DJWSegmentedViewController ()
+
+@end
+
+@implementation DJWSegmentedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
