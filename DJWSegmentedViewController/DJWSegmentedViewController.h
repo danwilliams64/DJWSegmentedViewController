41f5e94fa1e416017922830dff4a1f2b70dc3a0e
Dan Williams <dan@danwilliams.co>
2014-05-18 09:28:34 +0100

    Adds animation between view controllers.

--- DJWSegmentedViewController/DJWSegmentedViewController.h
+++ DJWSegmentedViewController/DJWSegmentedViewController.h
@@ -12,14 +12,41 @@
 
 @protocol DJWSegmentedViewControllerDataSource <NSObject>
 
+/**
+ *  The number of viewControllers the view is responsible for managing.
+ */
 - (NSUInteger)numberOfViewControllers;
+
+/**
+ *  Supply the DJWSegmentedViewController instance with a view Controller for display, at the given index.
+ *
+ *  @param segmentedViewController The DJWSegmentedViewController.
+ *  @param index                   Index of required viewController.
+ *
+ *  @return The view controller for display.
+ */
 - (UIViewController *)DJWSegmentedViewController:(DJWSegmentedViewController *)segmentedViewController viewControllerAtIndex:(NSUInteger)index;
+
+/**
+ *  Supply the DJWSegmentedViewController instance with a title for display in the segmented control.
+ *
+ *  @param segmentedViewController The DJWSegmentedViewController
+ *  @param index                   Index of required title.
+ *
+ *  @return The title for display on the segmented control.
+ */
 - (NSString *)DJWSegmentedViewController:(DJWSegmentedViewController *)segmentedViewController segmentedControlTitleForIndex:(NSUInteger)index;
 
 @end
 
 @protocol DJWSegmentedViewControllerDelegate <NSObject>
 @optional
+/**
+ *  Notifies the delegate that the DJWSegmentedViewController did move to displaying the view controller at index specified.
+ *
+ *  @param segmentedViewController The DJWSegmentedViewController
+ *  @param newIndex                Index of the viewController now being shown.
+ */
 - (void)DJWSegmentedViewController:(DJWSegmentedViewController *)segmentedViewController didMoveToViewControllerAtIndex:(NSUInteger)newIndex;
 
 @end
@@ -35,10 +62,32 @@ typedef NS_ENUM(NSUInteger, DJWSegmentedViewControllerControlPlacement)
 
 @property (nonatomic, weak) id <DJWSegmentedViewControllerDataSource> dataSource;
 @property (nonatomic, weak) id <DJWSegmentedViewControllerDelegate> delegate;
-@property (nonatomic, assign, readonly) NSUInteger currentViewControllerIndex;
 
+/**
+ *  The displayed view controller's index.
+ */
+@property (nonatomic, assign, readonly) NSUInteger currentViewControllerIndex;
+/**
+ *  Enables a swipe gesture to switch between view controllers. Defaults to `NO`.
+ */
+@property (nonatomic, assign, getter = isSwipeGestureEnabled) BOOL swipeGestureEnabled;
+/**
+ *  Enables the viewController transition animation. Defaults to `YES`.
+ */
+@property (nonatomic, assign) BOOL animatedViewControllerTransitionAnimationEnabled;
+
+/**
+ *  Instantiates a new instance of DJWSegmentedViewController with the specified segmented control placement. The instance should be displayed within a Navigation Controller.
+ *
+ *  @param placement See DJWSegmentedViewControllerPlacement ENUM for possible values.
+ *
+ *  @return Instance of DJWSegmentedViewController
+ */
 - (instancetype)initWithControlPlacement:(DJWSegmentedViewControllerControlPlacement)placement;
 
+/**
+ *  Reloads the view controller, and returns the `currentViewControllerIndex` value to `0`.
+ */
 - (void)reload;
 
 @end
--- DJWSegmentedViewController/DJWSegmentedViewController.m
+++ DJWSegmentedViewController/DJWSegmentedViewController.m
@@ -8,10 +8,18 @@
 
 #import "DJWSegmentedViewController.h"
 
+typedef NS_ENUM(NSUInteger, DJWSegmentedViewControllerTransitionDirection) {
+    DJWSegmentedViewControllerTransitionDirectionLeft,
+    DJWSegmentedViewControllerTransitionDirectionRight
+};
+
 @interface DJWSegmentedViewController ()
 @property (nonatomic, strong) UISegmentedControl *segmentedControl;
 @property (nonatomic, assign) DJWSegmentedViewControllerControlPlacement placement;
 @property (nonatomic, assign, readwrite) NSUInteger currentViewControllerIndex;
+@property (nonatomic, assign) NSUInteger previousViewControllerIndex;
+@property (nonatomic, strong) UISwipeGestureRecognizer *swipeGestureRecogniserLeft;
+@property (nonatomic, strong) UISwipeGestureRecognizer *swipeGestureRecogniserRight;
 @end
 
 @implementation DJWSegmentedViewController
@@ -20,6 +28,12 @@
 {
     if (self = [super init]) {
         self.placement = placement;
+        _swipeGestureEnabled = YES;
+        _animatedViewControllerTransitionAnimationEnabled = YES;
+        _swipeGestureRecogniserLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
+        _swipeGestureRecogniserLeft.direction = UISwipeGestureRecognizerDirectionLeft;
+        _swipeGestureRecogniserRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeGesture:)];
+        _swipeGestureRecogniserRight.direction = UISwipeGestureRecognizerDirectionRight;
     }
     return self;
 }
@@ -61,10 +75,17 @@
 
 #pragma mark - Private
 
+
+
+
 - (void)addViewControllerViewAsViewAtIndex:(NSInteger)index
 {
-    [self removeCurrentViewControllerView];
+    UIView *previousSnapShot = [self.view.subviews.firstObject snapshotViewAfterScreenUpdates:NO];
+    [self removePreviousViewController];
+    [self.view addSubview:previousSnapShot];
     
+    DJWSegmentedViewControllerTransitionDirection direction = (self.previousViewControllerIndex < index) ?  DJWSegmentedViewControllerTransitionDirectionLeft : DJWSegmentedViewControllerTransitionDirectionRight;
+
     UIViewController *viewController = [self.dataSource DJWSegmentedViewController:self viewControllerAtIndex:index];
     if (!viewController) {
         [[NSException exceptionWithName:@"View Controller is nil" reason:@"The view controller returned from DJWSegmentedViewController:viewControllerAtIndex is nil." userInfo:nil] raise];
@@ -72,13 +93,40 @@
     }
     
     [self addChildViewController:viewController];
-    [self.view addSubview:viewController.view];
+    UIView *viewControllerView = viewController.view;
+    CGRect startFrame = CGRectOffset(viewControllerView.frame, (direction == DJWSegmentedViewControllerTransitionDirectionLeft) ? CGRectGetWidth(viewControllerView.frame) : -CGRectGetWidth(viewControllerView.frame), 0);
+    viewControllerView.frame = startFrame;
+    [self.view addSubview:viewControllerView];
     [self didMoveToParentViewController:self];
     
-    [self.delegate DJWSegmentedViewController:self didMoveToViewControllerAtIndex:index];
+    if (!previousSnapShot || !self.animatedViewControllerTransitionAnimationEnabled) {
+        [UIView setAnimationsEnabled:NO];
+        previousSnapShot = [UIView new];
+    }
+    
+    [self animateViewsControllersForInitialDisplay:@[previousSnapShot, viewControllerView] inDirection:direction withXOffset:CGRectGetWidth(viewControllerView.frame) withCompletionCallback:^{
+        [previousSnapShot removeFromSuperview];
+        [self.delegate DJWSegmentedViewController:self didMoveToViewControllerAtIndex:index];
+    }];
+    
+    [UIView setAnimationsEnabled:YES];
 }
 
-- (void)removeCurrentViewControllerView
+- (void)animateViewsControllersForInitialDisplay:(NSArray *)views inDirection:(DJWSegmentedViewControllerTransitionDirection)direction withXOffset:(CGFloat)xOffset withCompletionCallback:(void(^)())callback;
+{
+    [views enumerateObjectsUsingBlock:^(UIView *view, NSUInteger idx, BOOL *stop) {
+        [UIView animateWithDuration:0.6 delay:0 usingSpringWithDamping:1.0 initialSpringVelocity:0.2 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            CGFloat originX = (direction == DJWSegmentedViewControllerTransitionDirectionLeft) ? view.frame.origin.x - xOffset : view.frame.origin.x + xOffset;
+            view.frame = CGRectMake(originX, view.frame.origin.y, CGRectGetWidth(view.frame), CGRectGetHeight(view.frame));
+        } completion:^(BOOL finished) {
+            if (callback) {
+                callback();
+            }
+        }];
+    }];
+}
+
+- (void)removePreviousViewController
 {
     if (self.view.subviews.count == 0) {
         return;
@@ -86,16 +134,37 @@
     
     UIViewController *currentViewController = [self.dataSource DJWSegmentedViewController:self viewControllerAtIndex:self.currentViewControllerIndex];
     [currentViewController removeFromParentViewController];
-    [self.view.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
     [self didMoveToParentViewController:self];
 }
 
 #pragma mark - Target Action
 
+- (void)handleSwipeGesture:(UISwipeGestureRecognizer *)sender
+{
+    switch (sender.direction) {
+        case UISwipeGestureRecognizerDirectionLeft:
+        {
+            if (self.currentViewControllerIndex + 1 != self.segmentedControl.numberOfSegments - 1) {
+                self.currentViewControllerIndex++;
+            }
+        }
+        case UISwipeGestureRecognizerDirectionRight:
+        {
+            if (self.currentViewControllerIndex - 1 != 0) {
+                self.currentViewControllerIndex--;
+            }
+        }
+            
+        default:
+            break;
+    }
+}
+
 - (void)segmentedControlValueChanged:(UISegmentedControl *)sender
 {
     NSInteger index = sender.selectedSegmentIndex;
-    [self addViewControllerViewAsViewAtIndex:index];
+    self.previousViewControllerIndex = _currentViewControllerIndex;
+    self.currentViewControllerIndex = index;
 }
 
 #pragma mark - Getters
@@ -114,6 +183,7 @@
 - (void)setCurrentViewControllerIndex:(NSUInteger)currentViewControllerIndex
 {
     _currentViewControllerIndex = currentViewControllerIndex;
+    [self.segmentedControl setSelectedSegmentIndex:_currentViewControllerIndex];
     [self addViewControllerViewAsViewAtIndex:_currentViewControllerIndex];
 }
 
